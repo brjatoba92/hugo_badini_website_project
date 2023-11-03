7a9aab34856704e7cef4b3721565bfea5ee2a803
brjatoba92 <brjatoba92@gmail.com>
2023-11-03 18:57:34 -0300

    create the new template site - stop in cards aim div

--- index.js
+++ index.js
@@ -5,3 +5,13 @@ function onClickScroll(id){
 function onClickLink(url){
     window.open(url)
 }
+
+const btnMobile = document.querySelector('btn-mobile')
+
+
+const nav = document.getElementById('nav')
+
+
+btnMobile.addEventListener('click', () =>{
+    nav.classList.add('active')
+})
